a7e65a32a144190a020c83d044eb75de1b69a7d7
Era Fejza <102391145+EraFejza@users.noreply.github.com>
2024-12-31 15:02:29 +0100

    Delete LibraryManager directory

--- LibraryManager/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module project.librarymanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens project.librarymanager to javafx.fxml;
-    exports project.librarymanager;
-}
